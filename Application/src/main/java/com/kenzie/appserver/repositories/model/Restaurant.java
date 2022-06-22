87e3f8a1bffe3e3e97d026d70072092006ea1093
Shirley Tong <msshirleytong@gmail.com>
2022-06-21 22:42:39 -0400

    restaurantModel

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/Restaurant.java
@@ -0,0 +1,84 @@
+package com.kenzie.appserver.repositories.model;
+
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.List;
+
+@DynamoDBTable(tableName = "Restaurant")
+public class Restaurant {
+
+    private String restaurantId;
+    private int rating;
+    private String restaurantName;
+    private String status;
+    private String cuisine;
+    private String location;
+    private List<String> menu;
+
+    @DynamoDBHashKey(attributeName = "RestaurantId")
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    @DynamoDBRangeKey(attributeName = "Rating")
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @DynamoDBAttribute(attributeName = "RestaurantName")
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    @DynamoDBAttribute(attributeName = "Status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @DynamoDBAttribute(attributeName = "Cuisine")
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    @DynamoDBAttribute(attributeName = "Location")
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @DynamoDBAttribute(attributeName = "Menu")
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<String> menu) {
+        this.menu = menu;
+    }
+}
