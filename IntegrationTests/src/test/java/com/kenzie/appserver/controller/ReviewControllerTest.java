84ec9c6df7719782ac8c1862a7c19e8d2f706bab
Eric Tun <ERICLYNNTUN@GMAIL.COM>
2022-06-24 15:04:58 -0400

    reviewcontroller integration test d

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/ReviewControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/ReviewControllerTest.java
@@ -1,12 +1,18 @@
 package com.kenzie.appserver.controller;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kenzie.appserver.IntegrationTest;
 import net.andreinc.mockneat.MockNeat;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IntegrationTest
@@ -18,5 +24,34 @@ public class ReviewControllerTest {
     private final ObjectMapper mapper = new ObjectMapper();
     private final MockNeat mockNeat = MockNeat.threadLocal();
 
-    //QueryUtility?
+    @Test
+    public void can_create_review() throws Exception {
+        String restaurantId = "3";
+        String userId = mockNeat.strings().valStr();
+        String review = mockNeat.strings().valStr();
+        int rating = 4;
+
+        ReviewCreateRequest reviewCreateRequest = new ReviewCreateRequest();
+        reviewCreateRequest.setRestaurantId(restaurantId);
+        reviewCreateRequest.setUserId(userId);
+        reviewCreateRequest.setReview(review);
+        reviewCreateRequest.setRating(rating);
+
+        //hopefully fixes with all classes implemented otherwise idk
+        mapper.registerModule(new JavaTimeModule());
+
+        mvc.perform(post("/review")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(reviewCreateRequest)))
+                .andExpect(jsonPath("restaurantId")
+                        .value(is(restaurantId)))
+                .andExpect(jsonPath("userId")
+                        .value(is(userId)))
+                .andExpect(jsonPath("review")
+                        .value(is(review)))
+                .andExpect(jsonPath("rating")
+                        .value(is(rating)))
+                .andExpect(status().isCreated());
+    }
 }
