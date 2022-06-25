c3f5616d4a7dd7040d136e37cf0769d3aa93c3fd
Marc-Dwayne Santos <marcdwayne.santos@mykenzie.snhu.edu>
2022-06-25 01:24:53 -0400

    RestaurantControllerTests

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/RestaurantControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/RestaurantControllerTest.java
@@ -2,19 +2,25 @@ package com.kenzie.appserver.controller;
 
 import com.kenzie.appserver.IntegrationTest;
 import com.kenzie.appserver.controller.model.ExampleCreateRequest;
+import com.kenzie.appserver.controller.model.RestaurantCreateRequest;
 import com.kenzie.appserver.service.RestaurantService;
 import com.kenzie.appserver.service.model.Restaurant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import net.andreinc.mockneat.MockNeat;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.management.Query;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static java.util.UUID.randomUUID;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,18 +37,25 @@ class RestaurantControllerTest {
     RestaurantService restaurantService;
 
     private final MockNeat mockNeat = MockNeat.threadLocal();
+    private QueryUtility queryUtility;
 
     private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeAll
+    public void setup() {
+        queryUtility = new QueryUtility(mvc);
+    }
 
     @Test
     public void getById_Exists() throws Exception {
         String id = UUID.randomUUID().toString();
-        String name = mockNeat.strings().valStr();
+        String name = mockNeat.strings().get();
 
-        Restaurant restaurant = new Restaurant(id, name);
-        Restaurant persistedRestaurant = restaurantService.addNewExample(restaurant);
-        mvc.perform(get("/example/{id}", persistedRestaurant.getId())
-                        .accept(MediaType.APPLICATION_JSON))
+        RestaurantCreateRequest restaurantCreateRequest = new RestaurantCreateRequest();
+        restaurantCreateRequest.setRestaurantId(id);
+        restaurantCreateRequest.setRestaurantName(name);
+
+        queryUtility.restaurantControllerClient.getById(restaurantCreateRequest.getName())
                 .andExpect(jsonPath("id")
                         .value(is(id)))
                 .andExpect(jsonPath("name")
@@ -51,22 +64,48 @@ class RestaurantControllerTest {
     }
 
     @Test
-    public void createExample_CreateSuccessful() throws Exception {
-        String name = mockNeat.strings().valStr();
+    public void getById_invalidIdIsEmpty() throws Exception {
+        String id = "";
+        String name = mockNeat.strings().get();
 
-        ExampleCreateRequest exampleCreateRequest = new ExampleCreateRequest();
-        exampleCreateRequest.setName(name);
+        RestaurantCreateRequest restaurantCreateRequest = new RestaurantCreateRequest();
+        restaurantCreateRequest.setRestaurantId(id);
+        restaurantCreateRequest.setRestaurantName(name);
 
-        mapper.registerModule(new JavaTimeModule());
-
-        mvc.perform(post("/example")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(exampleCreateRequest)))
-                .andExpect(jsonPath("id")
-                        .exists())
-                .andExpect(jsonPath("name")
-                        .value(is(name)))
-                .andExpect(status().isCreated());
+        queryUtility.restaurantControllerClient.getById(restaurantCreateRequest.getName())
+                .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void getById_invalidIdIsNull() throws Exception {
+        String id = null;
+        String name = mockNeat.strings().get();
+
+        RestaurantCreateRequest restaurantCreateRequest = new RestaurantCreateRequest();
+        restaurantCreateRequest.setRestaurantId(id);
+        restaurantCreateRequest.setRestaurantName(name);
+
+        queryUtility.restaurantControllerClient.getById(restaurantCreateRequest.getName())
+                .andExpect(status().isBadRequest());
+    }
+
+//    @Test
+//    public void createExample_CreateSuccessful() throws Exception {
+//        String name = mockNeat.strings().valStr();
+//
+//        ExampleCreateRequest exampleCreateRequest = new ExampleCreateRequest();
+//        exampleCreateRequest.setName(name);
+//
+//        mapper.registerModule(new JavaTimeModule());
+//
+//        mvc.perform(post("/example")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(mapper.writeValueAsString(exampleCreateRequest)))
+//                .andExpect(jsonPath("id")
+//                        .exists())
+//                .andExpect(jsonPath("name")
+//                        .value(is(name)))
+//                .andExpect(status().isCreated());
+//    }
 }
