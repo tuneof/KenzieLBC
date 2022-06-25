e94aa20594684a7aa8ab09e7ad5e9e6695c2fda5
Shirley Tong <msshirleytong@gmail.com>
2022-06-24 22:35:17 -0400

    reviewservice

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.kenzie.appserver.repositories;
+
+import com.kenzie.appserver.repositories.model.RestaurantRecord;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface ReviewRepository extends CrudRepository<RestaurantRecord, String> {
+}
---
+++ Application/src/main/java/com/kenzie/appserver/service/ReviewService.java
@@ -0,0 +1,54 @@
+package com.kenzie.appserver.service;
+
+import com.kenzie.appserver.repositories.RestaurantRepository;
+import com.kenzie.appserver.repositories.ReviewRepository;
+import com.kenzie.appserver.service.model.Restaurant;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ReviewService {
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public Review findById(String id) {
+        Review reviewFromBackend = reviewRepository
+                .findById(id)
+                .map(restaurant -> new Restaurant(restaurant.getRestaurantId(), restaurant.getRestaurantName(),
+                        restaurant.getRating(), restaurant.getStatus(), restaurant.getCuisine(), restaurant.getLocation(),
+                        restaurant.getMenu()))
+                .orElse(null);
+
+        return restaurantFromBackend;
+    }
+
+    public List<Restaurant> findAll() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurantRepository
+                .findAll()
+                .forEach(restaurant -> restaurants
+                        .add(new Restaurant(
+                                restaurant.getRestaurantId(),
+                                restaurant.getRestaurantName(),
+                                restaurant.getRating(),
+                                restaurant.getStatus(),
+                                restaurant.getCuisine(),
+                                restaurant.getLocation(),
+                                restaurant.getMenu())));
+
+        return restaurants;
+    }
+
+//    public Restaurant addNewExample(Restaurant restaurant) {
+//        ExampleRecord exampleRecord = new ExampleRecord();
+//        exampleRecord.setId(restaurant.getId());
+//        exampleRecord.setName(restaurant.getName());
+//        restaurantRepository.save(exampleRecord);
+//        return restaurant;
+//    }
+}
