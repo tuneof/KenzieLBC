44d0cf3c7c30e2029e04a8b51480c77b7511ffc4
Eric Tun <ERICLYNNTUN@GMAIL.COM>
2022-07-13 15:28:32 -0400

    small visual change

--- Frontend/src/pages/reviewPage.js
+++ Frontend/src/pages/reviewPage.js
@@ -45,10 +45,10 @@ class ReviewPage extends BaseClass {
                 }
 
                 reviewHTML += `<li>
-                <h4>Restaurant Id: ${review.restaurantId}</h4>
-                <div>User Id: ${review.userId}</div>
-                <div>Rating: ${review.rating}</div>
-                <div>Review: ${review.review}</div>
+                <h4>Restaurant ID: ${review.restaurantId}</h4>
+                <div>By User ID: ${review.userId}</div>
+                <div>Rating for Restaurant: ${review.rating}</div>
+                <div>Review Written: ${review.review}</div>
                 </li>`;
             }
             resultArea.innerHTML = reviewHTML + "</ul>";
